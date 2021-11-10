6d519903b9d516ec32a945f8e2f58e317f16b02a
Adrián Bernardi <adrian.bernardi@gmail.com>
2021-11-09 22:14:23 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-# This file is part of Tryton.  The COPYRIGHT file at the top level of
-# this repository contains the full copyright notices and license terms.
+# This file is part of the sale_subscription_recompute_price module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 import io
 import os
 import re
 from configparser import ConfigParser
-from setuptools import setup, find_packages
+from setuptools import setup
+
+MODULE = 'sale_subscription_recompute_price'
+PREFIX = 'trytonar'
+MODULE2PREFIX = {}
 
 
 def read(fname):
@@ -16,6 +21,10 @@ def read(fname):
 
 
 def get_require_version(name):
+    #if name.startswith('trytonar_'):
+        #return ''
+    if name in LINKS:
+        return '%s@%s' % (name, LINKS[name])
     if minor_version % 2:
         require = '%s >= %s.%s.dev0, < %s.%s'
     else:
@@ -35,28 +44,30 @@ version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = 'trytonar_sale_subscription_recompute_price'
-
-download_url = 'http://downloads.tryton.org/%s.%s/' % (
-    major_version, minor_version)
+series = '%s.%s' % (major_version, minor_version)
 if minor_version % 2:
-    version = '%s.%s.dev0' % (major_version, minor_version)
-    download_url = (
-        'hg+http://hg.tryton.org/modules/%s#egg=%s-%s' % (
-            name[8:], name, version))
+    branch = 'master'
+else:
+    branch = series
+
+download_url = 'https://github.com/gcoop-libre/sale_subscription_recompute_price/tree/%s' % branch
+
+LINKS = {}
 
 requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
-        requires.append(get_require_version('trytond_%s' % dep))
+        module_name = '%s_%s' % (MODULE2PREFIX.get(dep, 'trytond'), dep)
+        requires.append(get_require_version(module_name))
+
 requires.append(get_require_version('trytond'))
 
 tests_require = [get_require_version('proteus')]
-dependency_links = []
+dependency_links = list(LINKS.values())
 if minor_version % 2:
     dependency_links.append('https://trydevpi.tryton.org/')
 
-setup(name=name,
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
     description='',
     long_description=read('README.rst'),
@@ -71,15 +82,15 @@ setup(name=name,
         "Source Code": 'https://hg.tryton.org/modules/sale_subscription_recompute_price',
         },
     keywords='sale subscription recompute price',
-    package_dir={'trytond.modules.sale_subscription_recompute_price': '.'},
-    packages=(
-        ['trytond.modules.sale_subscription_recompute_price'] +
-        ['trytond.modules.sale_subscription_recompute_price.%s' % p for p in find_packages()]
-        ),
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
+    packages=[
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
+        ],
     package_data={
-        'trytond.modules.sale_subscription_recompute_price': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
-                'icons/*.svg', 'tests/*.rst']),
+        'trytond.modules.%s' % MODULE: (info.get('xml', []) + [
+            'tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
+            '*.fods', 'icons/*.svg', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -88,41 +99,30 @@ setup(name=name,
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Natural Language :: Bulgarian',
-        'Natural Language :: Catalan',
-        'Natural Language :: Chinese (Simplified)',
-        'Natural Language :: Czech',
-        'Natural Language :: Dutch',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
-        'Natural Language :: French',
-        'Natural Language :: German',
-        'Natural Language :: Hungarian',
-        'Natural Language :: Italian',
-        'Natural Language :: Persian',
-        'Natural Language :: Polish',
-        'Natural Language :: Portuguese (Brazilian)',
-        'Natural Language :: Russian',
-        'Natural Language :: Slovenian',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
+        'Topic :: Office/Business :: Financial :: Accounting',
         ],
     license='GPL-3',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     install_requires=requires,
     dependency_links=dependency_links,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    sale_subscription_recompute_price = trytond.modules.sale_subscription_recompute_price
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
